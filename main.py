d1501c0451a0b95400d176f2f3c5b7fd685f6f74
Ezequiel <ezequielw22l@gmail.com>
2026-05-25 00:25:30 -0300

    .

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def create_app() -> FastAPI:
 
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[origenes_permitidos],
+        allow_origins=origenes_permitidos,
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
